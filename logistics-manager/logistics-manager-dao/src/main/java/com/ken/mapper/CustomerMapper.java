ea60de049d178f63feedce532db445284b62705c
ken <13058053558@163.com>
2022-07-12 23:27:37 +0800

    逆向工程代码生成提交

---
+++ logistics-manager/logistics-manager-dao/src/main/java/com/ken/mapper/CustomerMapper.java
@@ -0,0 +1,31 @@
+package com.ken.mapper;
+
+import com.ken.pojo.Customer;
+import com.ken.pojo.CustomerExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface CustomerMapper {
+    int countByExample(CustomerExample example);
+
+    int deleteByExample(CustomerExample example);
+
+    int deleteByPrimaryKey(Integer customerId);
+
+    int insert(Customer record);
+
+    int insertSelective(Customer record);
+
+    List<Customer> selectByExample(CustomerExample example);
+
+    Customer selectByPrimaryKey(Integer customerId);
+
+    int updateByExampleSelective(@Param("record") Customer record, @Param("example") CustomerExample example);
+
+    int updateByExample(@Param("record") Customer record, @Param("example") CustomerExample example);
+
+    int updateByPrimaryKeySelective(Customer record);
+
+    int updateByPrimaryKey(Customer record);
+}
